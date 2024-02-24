e27da37869507efed4f7aa675a4afe6ea7a0ce17
Jake Glick <jake.glick@student.moriah.nsw.edu.au>
2024-02-24 17:02:43 +1100

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,13 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
-      title: Text('my first app'),
+      title: Text('my first flutter app'),
       centerTitle: true,
       backgroundColor: Colors.red[600]
     ),
     body: Center(
-      child: Text(
-        'hello, ninjas!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
+      child: Image.asset('assets/one-77.jpg'),
     ),
-
     floatingActionButton: FloatingActionButton(
       onPressed: () => (),
       backgroundColor: Colors.red[600],
