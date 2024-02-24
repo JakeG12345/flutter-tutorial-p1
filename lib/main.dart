b7991cd10ff640c3221e18437d01cbd837fd5ad9
Jake Glick <jake.glick@student.moriah.nsw.edu.au>
2024-02-24 17:28:46 +1100

    tutorial 13 - shortcuts and flutter outline

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: Home(),
 ));
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('my first flutter app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600]
-    ),
-    body: Center(
-      child: IconButton(onPressed: () {print("you clicked me");}, icon: Icon(Icons.alternate_email, color: Colors.amber,))
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => (),
-      backgroundColor: Colors.red[600],
-      child: Text('click'),
-    ),
-  );
+      appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.pinkAccent,
+            child: Text('two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.amber,
+            child: Text('three'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Text('one'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
   }
 }
